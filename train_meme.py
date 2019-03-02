2bef58033322fa2729975f0e6e20c0a1b2067c53
Mark Filus <markfilus@pitt.edu>
2019-03-02 16:09:45 -0500

    train_meme saves model as model.pkl

--- train_meme.py
+++ train_meme.py
@@ -1,16 +1,20 @@
-import numpy as np
 import os
 from textgenrnn import textgenrnn
+import pickle
+import sys
+
 
 def generate(file_name):
     textgen = textgenrnn()
-    textgen.train_from_file(file_name, num_epochs=1)
-    textgen.generate()
+    textgen.train_from_file(file_name, num_epochs=1, return_as_list=True)
+    return textgen.generate()
+
 
 def load_data():
     path = os.path.join(os.getcwd(), 'sample_text.txt')
     text = open(path).read().lower()
     print('corpus length:', len(text))
+
 
 def get_img(img_name):
     path = os.getcwd() + "/samplememes/"
@@ -19,8 +23,10 @@ def get_img(img_name):
 
 
 def main():
-    file_name = os.path.join(os.getcwd(), 'sample_text.txt')
-    generate(file_name)
+    generate_list = generate(sys.argv[1])
+    with open('list.pkl', 'wb') as f:
+        pickle.dump(generate_list, f)
+
 
 if __name__ == '__main__':
     main()
